6b4d3fb3fd8522d33cd8d2e14a31c4a61cb75f49
LightStar <lightstar@hallea.ru>
2017-04-13 17:05:08 +0300

    Remove obsolete and add one new package-info.java.

---
+++ src/main/java/ru/lightstar/clinic/jdbc/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Classes used to work with JDBC.
+ *
+ * @author LightStar
+ * @since 0.0.1
+ */
+package ru.lightstar.clinic.jdbc;
--- src/main/java/ru/lightstar/clinic/store/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Access to model classes.
- *
- * @author LightStar
- * @since 0.0.1
- */
-package ru.lightstar.clinic.store;
